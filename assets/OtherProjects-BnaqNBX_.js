61840e3660db7fa34ed2286f8dc04bbea7c7dc24
Berke K. <51491204+BerkeKanlikilic@users.noreply.github.com>
2025-04-28 11:18:51 +0300

    Updates

--- assets/OtherProjects-BnaqNBX_.js
+++ assets/OtherProjects-BnaqNBX_.js
@@ -1,4 +1,4 @@
-import{P as t,a}from"./ProjectsList-BtJd-ka_.js";import{e as r,_ as n,c as l,a as o,d as c,r as p,o as h}from"./index-CjH7Hwcj.js";const g=[new t("other-1","Simple OS in Shell","/img/projects/other-1/other-1-icon.png",`
+import{P as t,a}from"./ProjectsList-BUaiOzWP.js";import{e as r,_ as n,c as l,a as o,d as c,r as p,o as h}from"./index-TwgrFm5A.js";const g=[new t("other-1","Simple Shell","/img/projects/other-1/other-1-icon.png",`
     <div class="paragraph">
         This project was developed as part of a university assignment at Nottingham Trent University. It is a basic shell application for Linux, offering functionalities like user creation, user classification, and other similar tasks. The program includes both built-in and external commands that mimic a traditional Linux shell environment.
     </div>
@@ -27,7 +27,7 @@ import{P as t,a}from"./ProjectsList-BtJd-ka_.js";import{e as r,_ as n,c as l,a a
         Source code available on <a href="https://github.com/BerkeKanlikilic/Simple-Shell" target="_blank">GitHub</a>.
         </div>
     </div>
-    `,"#9B1B30"),new t("other-2","Pool Table Simulation","/img/projects/other-2/other-2-icon.png",`
+    `,"#9B1B30",!1,!1,!1,!0,"","https://github.com/BerkeKanlikilic/Simple-Shell"),new t("other-2","Pool Table Simulation","/img/projects/other-2/other-2-icon.png",`
         <div class="paragraph">
             This project, developed in Processing, simulates a pool table where users can interact with various parameters, including table size, ball sizes, hit force, and friction. The simulation realistically models the physics of pool, allowing users to manipulate these factors to observe their effects on gameplay. The balls are detected when they fall into the pockets, providing a dynamic and interactive experience. The project showcases my ability to apply physics concepts in programming while creating engaging simulations.
         </div>
@@ -55,4 +55,4 @@ import{P as t,a}from"./ProjectsList-BtJd-ka_.js";import{e as r,_ as n,c as l,a a
             <img class="phone-screenshot" src="/img/projects/other-2/other-2-ss-3.png" alt="Tic-Tac-Toe Screenshot" />
             <img class="phone-screenshot" src="/img/projects/other-2/other-2-ss-4.png" alt="Tic-Tac-Toe Screenshot" />
         </div>
-        `,"#2E8B57")],d=r({name:"OtherProjects",components:{ProjectsList:a},data:function(){return{projects:g}}});function m(s,e,u,f,v,b){const i=p("ProjectsList");return h(),l("div",null,[e[0]||(e[0]=o("h1",null,"Apps and Tools",-1)),e[1]||(e[1]=o("div",{style:{"margin-bottom":"30px"}}," Here are some apps and tools that I have created or significantly contributed to. ",-1)),c(i,{projects:s.projects},null,8,["projects"])])}const T=n(d,[["render",m]]);export{T as default};
+        `,"#2E8B57",!1,!1,!1,!0,"","https://github.com/BerkeKanlikilic/Processing-Pool-Table-Simulation")],m=r({name:"OtherProjects",components:{ProjectsList:a},data:function(){return{projects:g}}});function d(s,e,u,f,v,b){const i=p("ProjectsList");return h(),l("div",null,[e[0]||(e[0]=o("h1",null,"Apps and Tools",-1)),e[1]||(e[1]=o("div",{style:{"margin-bottom":"30px"}}," Here are some apps and tools that I have created or significantly contributed to. ",-1)),c(i,{projects:s.projects},null,8,["projects"])])}const y=n(m,[["render",d]]);export{y as default};
